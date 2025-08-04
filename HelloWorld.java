2334563c6427552a9230ad540cac700590728382
saikiran9962 <saikiran9962@gmail.com>
2025-08-04 14:43:28 -0500

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,7 @@
-package com.example;
-
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, Jenkins! Maven build successful.");
         System.out.println("Hello, Jenkins! Sai Kiran have good GK.");
+        System.out.println("===================");    
     }
 }
